749ab11fb6513c09416e6e2adc81eb474bffac8a
Clovil <46985217+Clovil@users.noreply.github.com>
2021-05-24 12:37:32 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,16 @@
 import styles from './Footer.module.css'
 
+function NewLineText(props) {
+  const Text = props.text;
+                                       
+  return text.split('\n').map(str => <p>{str}</p>);
+}
+
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <div>
-          <p>Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you '\n' Shanghai Maluoke E-Commerce Co., Ltd.</p>
-        </div>
+          <NewLineText text={'Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you \n Shanghai Maluoke E-Commerce Co., Ltd.'}/>
       </footer>
     </>
   )
