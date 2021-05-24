19d6a3723b81af68fd574d2e1468dcd46f0841ff
Clovil <46985217+Clovil@users.noreply.github.com>
2021-05-24 11:50:31 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        <p>Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you</p>
+        <p>Shanghai Maluoke E-Commerce Co., Ltd.</p>
       </footer>
     </>
   )
